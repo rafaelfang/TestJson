fbfd6a293c5dd0a5bb2520448924e74b69ad7b8f
rafaelfang <cf797@mail.missouri.edu>
2015-03-31 20:37:17 -0500

    main driver class

--- runner.cpp
+++ runner.cpp
@@ -15,18 +15,22 @@ int main(int argc, char* argv[]) {
 	//export HHLIB=<install_dir>/lib/hh
 	//export HHLIB=~lihongb/Tools/hhsearch64/lib/hh
 	Blast blast1(argv[1]);
+	blast1.executeAlignment();
 	Blast blast2(argv[2]);
+	blast2.executeAlignment();
 	HHSearch64 hhsearch1(argv[3]);
+	hhsearch1.executeAlignment();
 	HHSearch64 hhsearch2(argv[4]);
-	AlignmentTool* alignmentPtr[4];
-	alignmentPtr[0] = &blast1;
-	alignmentPtr[1] = &blast2;
-	alignmentPtr[2] = &hhsearch1;
-	alignmentPtr[3] = &hhsearch2;
-	int i = 0;
-	for (i = 0; i < 4; i++) {
-		alignmentPtr[i]->jsonParser();
-	}
+	hhsearch2.executeAlignment();
+	//AlignmentTool* alignmentPtr[4];
+	//alignmentPtr[0] = &blast1;
+	//alignmentPtr[1] = &blast2;
+	//alignmentPtr[2] = &hhsearch1;
+	//alignmentPtr[3] = &hhsearch2;
+	//int i = 0;
+	//for (i = 0; i < 4; i++) {
+	//	alignmentPtr[i]->jsonParser();
+	//}
 
 }
 
